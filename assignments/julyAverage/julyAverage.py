05a24d1e80137a3cd16c0370b8b2054dcff1bdec
Cody Stoffel <stoffelcody15@gmail.com>
2024-08-22 10:47:09 -0600

    almost done with fist assignment

--- assignments/julyAverage/julyAverage.py
+++ assignments/julyAverage/julyAverage.py
@@ -1,8 +1,20 @@
 import csv
+import math
 
 with open('julydata.txt', newline='') as csvfile:
     count=10
     flowreader = csv.reader(csvfile, delimiter='\t', quotechar='"')
-    for i in range(0,30):
-        row = next(flowreader)
-        print('], ['.join(row))
+    total = 0
+    totalsq = 0
+    count = 0
+    for row in flowreader:
+        if "USGS" in row :
+            time=row[2]
+            flow=int(row[4])
+            total=total + flow
+            toatlsq += flow*flow
+            count+=1
+#           print(time,', ',flow)
+    mean=(total/count)
+    variance=sqrt(toatlsq/count-mean*mean)
+    print(count, ' ',mean,' ',variance)
