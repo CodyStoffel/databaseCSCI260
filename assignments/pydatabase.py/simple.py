348420a76cdf9b39da5e2fa879f4c8997ee574c0
Cody Stoffel <stoffelcody15@gmail.com>
2024-10-03 11:00:26 -0600

    class 10-30-24

---
+++ assignments/pydatabase.py/simple.py
@@ -0,0 +1,24 @@
+import psycopg2
+from addCourses import Add
+from delCourses import Delete
+from showCourses import Show
+from updateCourses import Update
+
+print("1) Add a course")
+print("2) Show curret Courses")
+print("3) Update a course")
+print("4) Delete a course")
+print("Any other number to quit")
+
+choice = input("Enter 1,2,3 or 4")
+
+if choice =="1":
+    Add()
+elif choice =="2":
+    Show()
+elif choice =="3":
+    Update()
+elif choice == "4":
+    Delete()
+else:
+    quit()
