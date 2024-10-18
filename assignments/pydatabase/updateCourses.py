f0f94823f43cddd2dcb0a6b4121fbcf35432d293
Cody Stoffel <stoffelcody15@gmail.com>
2024-10-17 18:52:29 -0600

    add, delete, and update courses

--- assignments/pydatabase/updateCourses.py
+++ assignments/pydatabase/updateCourses.py
@@ -5,22 +5,24 @@
 # psycopg2 +
 # python3
 
-import psycopg2
 from db import *
 
-def Update():
+
+def updateCourse(classNewNum,classDept,classNum):
     cursor = connectDB()
-
-    print("Updating a class number in the offered classes")
-    classDept=input("Please enter the class Department:")
-    classNum=input("Please enter the class Number:")
-    classNewNum=input("Please enter the new class Number:")
-
-    #Use Fstring Formatted String to add class and number to query
     query="update courses set number='%s' where class='%s' and number='%s'" %(classNewNum,classDept,classNum)
     print(query)
     cursor.execute(query)
     disconnectDB()
+
+def Update():
+    
+    print("Updating a class number in the offered classes")
+    classDept=input("Please enter the class Department:")
+    classNum=input("Please enter the class Number:")
+    classNewNum=input("Please enter the new class Number:")
+    updateCourse(classNewNum,classDept,classNum)
+    #Use Fstring Formatted String to add class and number to query
 
 if __name__ == "__main__":
     Update()
